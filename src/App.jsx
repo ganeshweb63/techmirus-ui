ac55c2353452905230883748c5268b070292a03d
Ganesh Asimshetti <ganeshweb63@gmail.com>
2025-03-11 23:58:02 +0530

    Set up tailwind and daisy ui.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="btn w-64 rounded-full">Button</button>
     </>
   );
 }
