52fe909f39d1f0dcb95610a104bc97291e30338d
Ganesh Asimshetti <ganeshweb63@gmail.com>
2025-03-12 22:53:42 +0530

    Implement login with backend.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,93 @@
+import axios from "axios";
+import { useState } from "react";
+
 const LogInPage = () => {
+  const [email, setEmail] = useState("user1@gmail.com");
+  const [password, setPassword] = useState("User1@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/login",
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      );
+      // TODO:
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
-      <h1>LoginPage</h1>
+      <div className="card card-dash bg-base-100 w-96 ">
+        <div className="card-body">
+          <h2 className="card-title">Login</h2>
+          {/*  */}
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              placeholder="mail@site.com"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
+
+          {/*  */}
+          <label className="input validator">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="text"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+
+          {/*  */}
+          <div className="card-actions justify-end">
+            <button onClick={handleLogin} className="btn btn-primary">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
